07232caccf8c029724cc4d0297535ddc1adae4a0
浩明 <122860593+qwe133efqg453g9@users.noreply.github.com>
2025-03-01 20:54:12 +0800

    Add files via upload
    这是一个2选1的选择器

---
+++ selector_two_to_oner.v
@@ -0,0 +1,9 @@
+
+//用数据流建模方式描述 2 选 1 选择器代码
+module m_mux21(a,b,s,y);
+  input   a,b,s;        // 声明3个wire型输入变量a,b,和s，其宽度为1位。
+  output  y;           // 声明1个wire型输出变量y，其宽度为1位。
+
+  assign  y = (~s&a)|(s&b);  // 实现电路的逻辑功能。
+
+endmodule
